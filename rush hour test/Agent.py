a44b0730556673572255844fef5ebb8e87ad3683
Leonardo Almeida <leonardoalmeida7@ua.pt>
2022-10-20 22:29:24 +0100

    aaaaa

--- rush hour test/Agent.py
+++ rush hour test/Agent.py
@@ -1,5 +1,5 @@
 import asyncio
-# from Functions import *
+from Functions import *
 
 # car = ( letter, x, y, orientation, length )
 # node = ( parent, grid, cars, action, cost )
@@ -76,22 +76,40 @@ class Agent:
         open_nodes = [self.root]
         nodes={ get_str(self.root[1])}
         counter=0
+        # win_counter=0
 
         while True:
             counter+=1
+            # open_nodes.sort(key=lambda x: x[4])
             node = open_nodes.pop(0)
 
             if test_win(node[1]):
                 print(f"Counter: {counter}")
+                print(f"Open nodes: {len(open_nodes)}")
                 print(f"Nodes: {len(nodes)}")
+                # print(f"Estimated cost: {node[4]}")
+
+                # win_counter+=1
+                # if win_counter==1:
+                    
                 return get_path(node)
 
             for new_node in get_new_nodes(node, self.size):
                 new_str = get_str(new_node[1])
                 if new_str not in nodes:
                     nodes.add(new_str)
+                    
                     open_nodes.append(new_node)
 
+                    # open_nodes.insert(0, new_node)
+                    # open_nodes.sort(key=lambda x: x[4])
+                    
+                    # for idx, open_node in enumerate(open_nodes):
+                    #     if new_node[4] < open_node[4]:
+                    #         open_nodes.insert(idx, new_node)
+                    #         break
+                    # else:
+                    #     open_nodes.append(new_node)
 
 
     def calculate_solution(self, path):
@@ -99,7 +117,7 @@ class Agent:
         Calculate solution
         """
 
-        print(f"Path: {path}")
+        # print(f"Path: {path}")
 
         for move in path:
             yield from self.next_actions(move)
@@ -124,14 +142,14 @@ class Agent:
         """
         Get the next keys
         """
-        print(self.current_cars)
+        # print(self.current_cars)
         car = [car for car in self.current_cars if car[0] == move[0]][0]
         coords= self.nearest_square(car)
 
-        print_grid(self.current_grid)
-        print(f"Cursor: {self.cursor}")
-        print(f"Selected: {self.selected}")
-        print(f"Target car: {car[0]} at {coords}")
+        # print_grid(self.current_grid)
+        # print(f"Cursor: {self.cursor}")
+        # print(f"Selected: {self.selected}")
+        # print(f"Target car: {car[0]} at {coords}")
 
         while True:
         
@@ -242,133 +260,141 @@ class Agent:
 
 
 
-from math import dist
+# from math import dist
 
-def get_path(node):
-    """
-    Get the path to the root node
-    """
-    if node[0] is None:
-        return []
-    return get_path(node[0]) + [node[3]]
-
-
-def test_win(grid):
-    """
-    Check if the game is won
-    """
-    return grid[2][5] == 'A'
+# def get_path(node):
+#     """
+#     Get the path to the root node
+#     """
+#     if node[0] is None:
+#         return []
+#     return get_path(node[0]) + [(node[3][0][0], node[3][1])]
 
 
-def get_grid(str, size):
-    """
-    Get the grid from a string
-    """
-    return [[*str[i*size[0]:i*size[0]+size[0]]] for i in range(size[1])]
+# def test_win(grid):
+#     """
+#     Check if the game is won
+#     """
+#     return grid[2][5] == 'A'
 
 
-def get_str(grid):
-    """
-    Get the string from the grid
-    """
-    return ''.join([''.join(i) for i in grid])
+# def get_grid(str, size):
+#     """
+#     Get the grid from a string
+#     """
+#     return [[*str[i*size[0]:i*size[0]+size[0]]] for i in range(size[1])]
 
 
-def print_grid(grid):
-    """
-    Print the grid
-    """
-    print()
-    for i in grid:
-        for j in i:
-            print(j, end=' ')
-        print()
-    print()
+# def get_str(grid):
+#     """
+#     Get the string from the grid
+#     """
+#     return ''.join([''.join(i) for i in grid])
 
 
-def get_cars(grid, size):
-    """
-    Get the cars from grid
-    """
-    cars = {}
-    for y in range(size[1]):
-        for x in range(size[0]):
-            if grid[y][x] != 'o' and grid[y][x] != 'x':
-                if y-1 >= 0 and grid[y-1][x] == grid[y][x]:
-                    if grid[y][x] not in cars:
-                        cars[grid[y][x]] = [x, y-1, 'v', 2]     # car is vertical
-                    else:
-                        cars[grid[y][x]][3] += 1                # car length is increased
-                elif x-1 >= 0 and grid[y][x-1] == grid[y][x]:
-                    if grid[y][x] not in cars:
-                        cars[grid[y][x]] = [x-1, y, 'h', 2]     # car is horizontal
-                    else:
-                        cars[grid[y][x]][3] += 1                # car length is increased
-    cars_ = [[i, *cars[i]] for i in cars]
-    # cars_.sort(key=lambda x: x[0])
-    return cars_
+# def print_grid(grid):
+#     """
+#     Print the grid
+#     """
+#     print()
+#     for i in grid:
+#         for j in i:
+#             print(j, end=' ')
+#         print()
+#     print()
 
 
-def move_car(car, direction, grid):
-    """
-    Move the car to a given direction
-    """
-    letter, x, y, _, length = car
-    match direction:
-        case 'a':                       # left
-            grid[y][x-1] = letter
-            grid[y][x+length-1] = 'o'
-        case 'd':                       # right
-            grid[y][x+length] = letter
-            grid[y][x] = 'o'
-        case 'w':                       # up
-            grid[y-1][x] = letter
-            grid[y+length-1][x] = 'o'
-        case 's':                       # down
-            grid[y+length][x] = letter
-            grid[y][x] = 'o'
+# def get_cars(grid, size):
+#     """
+#     Get the cars from grid
+#     """
+#     cars = {}
+#     for y in range(size[1]):
+#         for x in range(size[0]):
+#             if grid[y][x] != 'o' and grid[y][x] != 'x':
+#                 if y-1 >= 0 and grid[y-1][x] == grid[y][x]:
+#                     if grid[y][x] not in cars:
+#                         cars[grid[y][x]] = [x, y-1, 'v', 2]     # car is vertical
+#                     else:
+#                         cars[grid[y][x]][3] += 1                # car length is increased
+#                 elif x-1 >= 0 and grid[y][x-1] == grid[y][x]:
+#                     if grid[y][x] not in cars:
+#                         cars[grid[y][x]] = [x-1, y, 'h', 2]     # car is horizontal
+#                     else:
+#                         cars[grid[y][x]][3] += 1                # car length is increased
+#     cars_ = [[i, *cars[i]] for i in cars]
+#     # cars_.sort(key=lambda x: x[0])
+#     return cars_
 
 
-def get_new_nodes(parent, size):
-    """
-    Calculate new nodes
-    """
-    grid, cars, cost = parent[1], parent[2], parent[4]
-    for idx, car in enumerate(cars):
-        letter, x, y, orientation, length = car
-        match orientation:
-            case 'h':   # horizontal
-
-                if x > 0 and grid[y][x-1] == 'o':                   # move left
-
-                    new_grid = [[*i] for i in grid] # copy the grid
-                    move_car(car, 'a', new_grid)
-                    new_cars = (*cars[:idx] , (letter, x-1, y, orientation, length) , *cars[idx+1:]) # copy the cars with the change
-                    yield (parent, new_grid, new_cars, (letter, 'a'), cost+1)
-
-                if x+length < size[0] and grid[y][x+length] == 'o': # move right
-
-                    new_grid = [[*i] for i in grid] # copy the grid
-                    move_car(car, 'd', new_grid)
-                    new_cars = (*cars[:idx] , (letter, x+1, y, orientation, length) , *cars[idx+1:]) # copy the cars with the change
-                    yield (parent, new_grid, new_cars, (letter, 'd'), cost+1)
-
-            case 'v':   # vertical
-
-                if y > 0 and grid[y-1][x] == 'o':                   # move up
-
-                    new_grid = [[*i] for i in grid] # copy the grid
-                    move_car(car, 'w', new_grid)
-                    new_cars = (*cars[:idx] , (letter, x, y-1, orientation, length) , *cars[idx+1:]) # copy the cars with the change
-                    yield (parent, new_grid, new_cars, (letter, 'w'), cost+1)
-
-                if y+length < size[1] and grid[y+length][x] == 'o': # move down
-
-                    new_grid = [[*i] for i in grid] # copy the grid
-                    move_car(car, 's', new_grid)
-                    new_cars = (*cars[:idx] , (letter, x, y+1, orientation, length) , *cars[idx+1:]) # copy the cars with the change
-                    yield (parent, new_grid, new_cars, (letter, 's'), cost+1)
+# def move_car(car, direction, grid):
+#     """
+#     Move the car to a given direction
+#     """
+#     letter, x, y, _, length = car
+#     match direction:
+#         case 'a':                       # left
+#             grid[y][x-1] = letter
+#             grid[y][x+length-1] = 'o'
+#         case 'd':                       # right
+#             grid[y][x+length] = letter
+#             grid[y][x] = 'o'
+#         case 'w':                       # up
+#             grid[y-1][x] = letter
+#             grid[y+length-1][x] = 'o'
+#         case 's':                       # down
+#             grid[y+length][x] = letter
+#             grid[y][x] = 'o'
 
 
-def heuristic():
-    ...
+# def get_new_nodes(parent, size):
+#     """
+#     Calculate new nodes
+#     """
+#     grid, cars, cost = parent[1], parent[2], parent[4]
+#     for idx, car in enumerate(cars):
+#         letter, x, y, orientation, length = car
+#         match orientation:
+#             case 'h':   # horizontal
+
+#                 if x > 0 and grid[y][x-1] == 'o':                   # move left
+
+#                     new_grid = [[*i] for i in grid] # copy the grid
+#                     move_car(car, 'a', new_grid)
+#                     new_cars = (*cars[:idx] , (letter, x-1, y, orientation, length) , *cars[idx+1:]) # copy the cars with the change
+#                     yield (parent, new_grid, new_cars, (new_cars[idx], 'a'), cost + get_cost(parent[3][0], cars[idx]))
+
+#                 if x+length < size[0] and grid[y][x+length] == 'o': # move right
+
+#                     new_grid = [[*i] for i in grid] # copy the grid
+#                     move_car(car, 'd', new_grid)
+#                     new_cars = (*cars[:idx] , (letter, x+1, y, orientation, length) , *cars[idx+1:]) # copy the cars with the change
+#                     yield (parent, new_grid, new_cars, (new_cars[idx], 'd'), cost+ get_cost(parent[3][0], cars[idx]))
+
+#             case 'v':   # vertical
+
+#                 if y > 0 and grid[y-1][x] == 'o':                   # move up
+
+#                     new_grid = [[*i] for i in grid] # copy the grid
+#                     move_car(car, 'w', new_grid)
+#                     new_cars = (*cars[:idx] , (letter, x, y-1, orientation, length) , *cars[idx+1:]) # copy the cars with the change
+#                     yield (parent, new_grid, new_cars, (new_cars[idx], 'w'), cost+ get_cost(parent[3][0], cars[idx]))
+
+#                 if y+length < size[1] and grid[y+length][x] == 'o': # move down
+
+#                     new_grid = [[*i] for i in grid] # copy the grid
+#                     move_car(car, 's', new_grid)
+#                     new_cars = (*cars[:idx] , (letter, x, y+1, orientation, length) , *cars[idx+1:]) # copy the cars with the change
+#                     yield (parent, new_grid, new_cars, (new_cars[idx], 's'), cost+ get_cost(parent[3][0], cars[idx]))
+
+
+# def heuristic():
+#     ...
+
+# def get_cost(car1,car2):
+#     # return 1
+#     if car1 is None:
+#         return 2 + abs(car2[1] - 3) + abs(car2[2] - 3)
+#     if car1[0] == car2[0]:
+#         return 1
+#     return 2 + abs(car1[1] - car2[1]) + abs(car1[2] - car2[2])
--- rush hour test/agent_test.py
+++ rush hour test/agent_test.py
@@ -1,12 +1,18 @@
 import time
 from Agent import Agent
 from Functions import *
+import sys
+
+# car = ( letter, x, y, orientation, length )
+# node = ( parent, grid, cars, action, cost )
 
 def main():
 
+    sys.setrecursionlimit(1000000)
+
     state = {"level": 1, "selected":'', "dimensions": [6, 6], "cursor": [3, 3], "grid": "01 BBCCMxEEELMNAAKLoNooKFFoJGGoooJHHIIo 5"}
 
-    level = 21
+    level = 0
 
     agent = Agent()
 
@@ -20,6 +26,8 @@ def main():
 
             print("\nLevel: ", line.split(" ")[0])
 
+            agent.selected= ''
+            agent.cursor = [3, 3]
             agent.level = state["level"]
             agent.size = state["dimensions"]
             agent.current_grid = get_grid(line.split(" ")[1], agent.size)
@@ -33,9 +41,17 @@ def main():
 
             total_times += end-start
 
+            n_keys= 0
+            for key in agent.calculate_solution(path):
+                agent.simulate(key)
+                if agent.current_grid[2][5] == 'A':
+                    break
+                n_keys += 1
+
             print("Time: ", end-start)
-            print("Path: ", path)
+            # print("Path: ", path)
             print("Moves: ", len(path))
+            print("Number of keys: ", n_keys)
 
 
     print("Total Time: ", total_times)
